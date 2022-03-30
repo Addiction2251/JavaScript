122655bb09c5638623e031f4691c3737bc2aef76
Addiction2251 <sanskarsingh23092002@gmail.com>
2022-03-30 10:32:47 +0530

    Comments added

--- Question3.js
+++ Question3.js
@@ -1,14 +1,14 @@
-var num = 84697;
-var c = num;
-var rev = 0;
+var num = 84697; // Number to be reversed
+var c = num; // Variable to store the value of the original number
+var rev = 0; // Variable to store the reversed number.
 
-function reverse(num) {
+function reverse(num) { //Function to reverse the given Number.
     while (num > 0) {
         var d = num % 10;
         rev = (rev * 10) + d;
         num = parseInt(num / 10);
     }
 }
-reverse(num);
-console.log("Original Number :- ", c);
-console.log("Reversed Number :- ", rev);
+reverse(num); //Calling the Function
+console.log("Original Number :- ", c); //Displaying the original number.
+console.log("Reversed Number :- ", rev); //Displaying the Reversed number.
